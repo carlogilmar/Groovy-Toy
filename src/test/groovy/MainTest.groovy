2d445ed96a702e6717661e3552edb676e109a150
carlogilmar <carlogilmar12@gmail.com>
2018-07-31 23:11:01 -0500

    Initial setup

---
+++ src/test/groovy/MainTest.groovy
@@ -0,0 +1,12 @@
+import spock.lang.Specification
+
+class MainTest extends Specification {
+
+    def "Test division of numbers by 2"() {
+      given:
+      when:
+      expect:
+       2 == 3
+    }
+
+}
